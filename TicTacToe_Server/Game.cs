2754372963634fa51b9f2653231b65a398a73fa0
Visterious <stavyankov@ukr.net>
2021-03-01 20:11:16 +0200

    add dispose methods and comments to server. Yeah, in one week, sorry

--- TicTacToe_Server/Game.cs
+++ TicTacToe_Server/Game.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TicTacToe_Server
 {
-    class Game
+    class Game : IDisposable
     {
 
-        public List<Socket> players { get; set; } = new List<Socket>();
+        public List<Socket> players { get; set; } = new List<Socket>(); // list of player sockets in game
 
 
         public Game(Socket p1, Socket p2)
         {
+            // adding sockets to 'players'
             players.Add(p1);
             players.Add(p2);
             Console.WriteLine($"Game created. Players: {p1.RemoteEndPoint}, {p2.RemoteEndPoint}");
@@ -25,17 +24,19 @@ namespace TicTacToe_Server
             Console.WriteLine("Game ended.");
         }
 
+        // this method handle messages from a client
         public void Handler(Socket p)
         {
             try
             {
-                while (true)
+                while (true) // handle cycle
                 {
-
+                    // empty variables for recieved data
                     StringBuilder builder = new StringBuilder();
                     int bytes = 0;
                     byte[] data = new byte[256];
 
+                    // recieving data in cycle while a client sends bytes
                     do
                     {
                         bytes = p.Receive(data);
@@ -43,13 +44,15 @@ namespace TicTacToe_Server
                     }
                     while (p.Available > 0);
 
-                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString()); // message in string
 
+                    // if message == '.' then game ends and handle cycle ends
                     if (builder.ToString() == ".")
                     {
                         break;
                     }
 
+                    // send data to another players besides the sender
                     foreach (Socket player in players)
                     {
                         if (player != p)
@@ -64,6 +67,21 @@ namespace TicTacToe_Server
                 Console.WriteLine(ex.Message);
             }
             Console.WriteLine($"{p.RemoteEndPoint} leave");
+        }
+
+        // dispose methods. I don't think it actually works
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                players = null;
+            }
         }
     }
 }
--- TicTacToe_Server/Server.cs
+++ TicTacToe_Server/Server.cs
@@ -11,32 +11,35 @@ namespace TicTacToe_Server
 {
     class Server
     {
+        // address and port that server must listens
         private static string address = ConfigurationManager.AppSettings["address"];
         private static int port = int.Parse(ConfigurationManager.AppSettings["port"]);
 
-        static List<Socket> connections { get; set; } = new List<Socket>();
+        static List<Socket> connections { get; set; } = new List<Socket>(); // list of player sockets that connects to the server
 
         static void Main(string[] args)
         {
+            // create a listen socket
             IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
-
             Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
             try
             {
-                listenSocket.Bind(ipPoint);
+                listenSocket.Bind(ipPoint); // listen our ip:port
 
-                listenSocket.Listen(10);
+                listenSocket.Listen(10); // we can listen listen to 10 clients maximum
 
                 Console.WriteLine("Сервер запущен. Ожидание подключений...");
 
 
-                while (true)
+                while (true) // cycle of accepting connections
                 {
-                    Socket handler = listenSocket.Accept();
+                    Socket handler = listenSocket.Accept(); // accept connection
                     Console.WriteLine(handler.RemoteEndPoint);
 
-                    connections.Add(handler);
+                    connections.Add(handler); // add new connection(socket) to 'connections'
 
+                    // if count of connections >= 2 then start new game and remove this connections from 'connections'
                     if (connections.Count >= 2)
                     {
                         newGame(connections[0], connections[1]);
@@ -52,24 +55,30 @@ namespace TicTacToe_Server
             }
         }
 
+        // method that start new game
         async static void newGame(Socket p1, Socket p2)
         {
             await Task.Run(() =>
             {
-                Game game = new Game(p1, p2);
-                try
+                using (Game game = new Game(p1, p2)) // create a game
                 {
-                    p1.Send(Encoding.UTF8.GetBytes("X;O"));
-                    p2.Send(Encoding.UTF8.GetBytes("O;X"));
+                    try
+                    {
+                        // send to players their symbols 
+                        p1.Send(Encoding.UTF8.GetBytes("X;O"));
+                        p2.Send(Encoding.UTF8.GetBytes("O;X"));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    // start handling players messages in different threads
+                    Thread gameThread1 = new Thread(new ThreadStart(() => game.Handler(p1)));
+                    gameThread1.Start();
+                    Thread gameThread2 = new Thread(new ThreadStart(() => game.Handler(p2)));
+                    gameThread2.Start();
+                    gameThread1.Join();
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                Thread gameThread1 = new Thread(new ThreadStart(() => game.Handler(p1)));
-                gameThread1.Start();
-                Thread gameThread2 = new Thread(new ThreadStart(() => game.Handler(p2)));
-                gameThread2.Start();
             });
         }
     }
